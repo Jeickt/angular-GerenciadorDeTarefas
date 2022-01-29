e9ecd6670719816d337042f7834cb30eb9ed625d
Jeison Leandro Rückert <jeison.let@gmail.com>
2022-01-29 11:33:59 -0300

    Implementando a listagem de tarefas

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TarefaService, Tarefa } from '../shared';
+
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
-  styleUrls: ['./listar-tarefa.component.css']
+  styleUrls: ['./listar-tarefa.component.css'],
 })
 export class ListarTarefaComponent implements OnInit {
+  tarefas: Tarefa[];
 
-  constructor() { }
+  constructor(private tarefaService: TarefaService) {}
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodas();
+    this.tarefas = [
+      new Tarefa(1, 'Tarefa 1', false),
+      new Tarefa(2, 'Tarefa 2', true),
+    ];
   }
 
+  listarTodas(): Tarefa[] {
+    return this.tarefaService.listarTodas();
+  }
 }
